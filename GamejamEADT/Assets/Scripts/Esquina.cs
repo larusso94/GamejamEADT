aec52d78d7dc13e9403c73e906361353c9882e9b
tomateycebolla <51667384+tomateycebolla@users.noreply.github.com>
2019-11-23 13:45:22 +0100

    1231231

---
+++ GamejamEADT/Assets/Scripts/Esquina.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esquinaç : MonoBehaviour
+{
+
+    public Transform customPivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(customPivot.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
