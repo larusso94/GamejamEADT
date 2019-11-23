61dc52d4c4538f599408b281c0bee0f21e527516
tomateycebolla <51667384+tomateycebolla@users.noreply.github.com>
2019-11-23 13:08:13 +0100

    2132131

--- GamejamEADT/Assets/Scripts/CambioGravedad.cs
+++ GamejamEADT/Assets/Scripts/CambioGravedad.cs
@@ -6,22 +6,24 @@
 public class CambioGravedad : MonoBehaviour
 {
 
-    public GameObject Player;
-    float dist;
+    
+    public float dist;
 
     void Start(){
 
-        dist = Player.GetComponent<GravityController>.D;
+        dist = GetComponent<Rigidbody2D>().gravityScale;
 
     }
 
     void Update(){
 
             if(dist >= 0){
+
                 this.GetComponent<SpriteRenderer>().flipY = false;
 
             }
             if(dist <= 0){
+
                 this.GetComponent<SpriteRenderer>().flipY = true;
                 
             }
