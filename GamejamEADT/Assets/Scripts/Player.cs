b3c8c777325ba1168574f2db68e8d3661be0ddce
Victor Suarez <victor.suarezc@estudiants.urv.cat>
2019-11-23 15:55:03 +0100

    Player

--- GamejamEADT/Assets/Scripts/Player.cs
+++ GamejamEADT/Assets/Scripts/Player.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed,forcejump;
+    public float speed, forcejump, speedmax, aceleracion, frenada;
     public bool jumpup, jumpdown;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-    
+
 
     }
 
@@ -19,47 +19,41 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
-            gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            
+            if ((speed < speedmax) && (jumpup || jumpdown))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2((-speed - aceleracion), 0));
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
-
         if (Input.GetKey("right") || Input.GetKey("d"))
         {
-            gameObject.transform.Translate(speed *Time.deltaTime, 0, 0);
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            if ((speed < speedmax) && (jumpup || jumpdown))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2((speed + aceleracion), 0));
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
         }
         if (Input.GetKeyDown(KeyCode.Space) && jumpup)
         {
-            
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcejump));
             jumpup = false;
         }
         if (Input.GetKeyDown(KeyCode.Space) && jumpdown)
         {
-
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -forcejump));
+           gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -forcejump));
             jumpdown = false;
         }
 
     }
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "ground")
+        void OnCollisionEnter2D(Collision2D collision)
         {
-            jumpup = true;
-        }
-        if(collision.gameObject.tag == "techo")
-        {
-            jumpdown = true;
+            if (collision.gameObject.tag == "ground")
+            {
+                jumpup = true;
+            }
+            if (collision.gameObject.tag == "techo")
+            {
+                jumpdown = true;
+            }
         }
     }
-
-    /*void OnTriggerEnter(Collider other)
-    {
-        GameObject.Find("Subtitulo");
-        SetActive(true);
-        "TextoSub".GetComponent<Text>.text = Text;
-    }
-    */
-}
---
+++ GamejamEADT/Assets/Scripts/Subtitulo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Subtitulo : MonoBehaviour
+{
+/*
+    GameObject Subtitulo;
+    GameObject Textosub;
+    public string Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Subtitulo = GameObject.Find("Subtitulo");
+            Subtitulo.SetActive(true);
+            Textosub = GameObject.Find("Subtitulo");
+            Textosub.GetComponent<Text>().text = Text;
+
+        }
+       
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Subtitulo = GameObject.Find("Subtitulo");
+            Subtitulo.SetActive(false);
+            
+
+        }
+
+    }*/
+}
