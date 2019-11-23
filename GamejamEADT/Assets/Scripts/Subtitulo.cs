a1c881da2f917a2ba8fbe42a318a4cd3c289a436
tomateycebolla <51667384+tomateycebolla@users.noreply.github.com>
2019-11-23 16:34:46 +0100

    DWQDWQ

--- GamejamEADT/Assets/Scripts/Subtitulo.cs
+++ GamejamEADT/Assets/Scripts/Subtitulo.cs
@@ -1,42 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Subtitulo : MonoBehaviour
 {
-/*
-    GameObject Subtitulo;
-    GameObject Textosub;
+    public GameObject SubtituloGO;
+    public GameObject Textosub;
     public string Text;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    
-    void OnTriggerEnter(Collider other)
+   void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
-        {
-            Subtitulo = GameObject.Find("Subtitulo");
-            Subtitulo.SetActive(true);
-            Textosub = GameObject.Find("Subtitulo");
+        if(other.gameObject.tag == "Player"){
+           SubtituloGO.SetActive(true);
+            
             Textosub.GetComponent<Text>().text = Text;
-
         }
-       
+        
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            Subtitulo = GameObject.Find("Subtitulo");
-            Subtitulo.SetActive(false);
-            
-
+        if(other.gameObject.tag == "Player"){
+            SubtituloGO.SetActive(false);
         }
+        
+    }
 
-    }*/
 }
