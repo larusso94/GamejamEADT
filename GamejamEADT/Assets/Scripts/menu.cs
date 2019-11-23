ed8c731abea77d2357605e70a39603d93009414e
JonNavgar <58087804+JonNavgar@users.noreply.github.com>
2019-11-23 13:56:37 +0100

    Menu hecho
    jon

--- GamejamEADT/Assets/Scripts/menu.cs
+++ GamejamEADT/Assets/Scripts/menu.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menu : MonoBehaviour
-{
+{   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,10 @@ public class menu : MonoBehaviour
     // Update is called once per frame
     public void NewGame()
     {
-        
+        SceneManager.LoadSceneAsync(1);
     }
-    public void Settings()
+    public void Exit()
     {
-
+        Application.Quit();
     }
 }
