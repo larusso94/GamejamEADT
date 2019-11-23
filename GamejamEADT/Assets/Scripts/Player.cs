a0ae5c73d329161f99ca415881c4004538f45e3b
JonNavgar <58087804+JonNavgar@users.noreply.github.com>
2019-11-23 11:43:53 +0100

    Player
    completado

--- GamejamEADT/Assets/Scripts/Player.cs
+++ GamejamEADT/Assets/Scripts/Player.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Player : MonoBehaviour
 {
 
-    public float speed;
+    public float speed,forcejump;
+    public bool jump;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +20,28 @@ public class Player : MonoBehaviour
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
             gameObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
-            //* if (Input.GetKey("right") || Input.GetKey("d"))
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            
         }
+
         if (Input.GetKey("right") || Input.GetKey("d"))
         {
             gameObject.transform.Translate(speed *Time.deltaTime, 0, 0);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
-        
+        if (Input.GetKeyDown(KeyCode.Space) && jump)
+        {
+            
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcejump));
+            jump = false;
+        }
     }
-    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            jump = true;
+        }
+    }
+
 }
