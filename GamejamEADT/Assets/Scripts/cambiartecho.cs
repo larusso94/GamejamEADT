fee9750cb7f86ff1a73917a5e02dd04f7e64ecd5
tomateycebolla <51667384+tomateycebolla@users.noreply.github.com>
2019-11-23 17:30:09 +0100

    dawdawd

---
+++ GamejamEADT/Assets/Scripts/cambiartecho.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambiartecho : MonoBehaviour
+{
+    public GameObject Player;
+
+    void OnTriggerEnter2D(Collider2D other){
+
+		if(other.gameObject.tag == "techo"){
+
+			Player.GetComponent<GravityController>().ceil = other.gameObject;
+
+		}
+}
+}
