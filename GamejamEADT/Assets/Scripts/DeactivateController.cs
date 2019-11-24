7ce021ebe643098ea433e3d527898ee3707d10c7
Larusso94 <lautaro_russo@hotmail.com>
2019-11-24 11:57:10 +0100

    commit

---
+++ GamejamEADT/Assets/Scripts/DeactivateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other){
+    	if (other.gameObject.tag == "Player"){
+    		other.gameObject.GetComponent<Player>().enabled = false;
+    		other.gameObject.GetComponent<Animator>().SetBool("corriendo", false);
+    		other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+    	}
+    }
+}
